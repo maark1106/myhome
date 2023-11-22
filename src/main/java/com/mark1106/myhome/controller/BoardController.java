8af85797edd452f391d78b33bc35d08f9fd3c66c
maark1106 <mark1106@naver.com>
2023-11-22 21:53:14 +0900

    feat : 게시판 내용 수정(중복 제외)

--- src/main/java/com/mark1106/myhome/controller/BoardController.java
+++ src/main/java/com/mark1106/myhome/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.mark1106.myhome.controller;
 import com.mark1106.myhome.model.Board;
 import com.mark1106.myhome.repository.BoardRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/board")
@@ -26,8 +28,13 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model){
-        model.addAttribute("board", new Board());
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id == null){
+            model.addAttribute("board", new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board",board);
+        }
         return "board/form";
     }
 
