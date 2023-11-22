a529f338e1a0ca2bc9618e62bc226bd569aa9fd6
maark1106 <mark1106@naver.com>
2023-11-22 21:12:42 +0900

    feat : 게시판 내용 등록 controller

--- src/main/java/com/mark1106/myhome/controller/BoardController.java
+++ src/main/java/com/mark1106/myhome/controller/BoardController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,6 +27,13 @@ public class BoardController {
 
     @GetMapping("/form")
     public String form(Model model){
+        model.addAttribute("board", new Board());
         return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String formSubmit(@ModelAttribute Board board){
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
