033ca3ceb8ce00c5197423c43b2299fdb6195418
maark1106 <mark1106@naver.com>
2023-11-23 10:55:45 +0900

    feat : BoardApiController 추가

--- src/main/java/com/mark1106/myhome/repository/BoardRepository.java
+++ src/main/java/com/mark1106/myhome/repository/BoardRepository.java
@@ -1,8 +1,11 @@
 package com.mark1106.myhome.repository;
 
 import com.mark1106.myhome.model.Board;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
